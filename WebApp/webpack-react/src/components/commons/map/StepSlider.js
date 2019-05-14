ac6032d42202163667a4fe674ed1e859bf70c165
zijies1 <546641464@qq.com>
2019-05-15 03:27:01 +1000

    Update StepSlider.js

--- WebApp/webpack-react/src/components/commons/map/StepSlider.js
+++ WebApp/webpack-react/src/components/commons/map/StepSlider.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/lab/Slider';
-import { cyan500 } from 'material-ui/styles/colors';
 import {setTime} from "../../actions";
 import {connect} from "react-redux";
 
